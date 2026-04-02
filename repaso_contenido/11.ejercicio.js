aa3358f4a728dd330322cc4f14011f1f845145be
Jorge <jltamayocabello@gmail.com>
2026-04-02 11:32:39 -0300

    Se añade nuevo ejemplo a ejercicio 11

--- repaso_contenido/11.ejercicio.js
+++ repaso_contenido/11.ejercicio.js
@@ -1,9 +1,13 @@
 let valores = [5, 6, 7, 6, 9];
 
-let total = 0;
+// let total = 0;
 
-for (const valor of valores) {
-    total+= valor;
-}
+// for (const valor of valores) {
+//    total+= valor;
+// }
+
+// console.log(total);
+
+total = valores.reduce((a, b) => a + b);
 
 console.log(total);
