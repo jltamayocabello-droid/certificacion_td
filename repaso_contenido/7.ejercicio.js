241ee4bc03ad620a5bddad0e4ac8f9a1798f9da8
Jorge <jltamayocabello@gmail.com>
2026-04-02 11:04:07 -0300

    Ejercicio 7

---
+++ repaso_contenido/7.ejercicio.js
@@ -0,0 +1,17 @@
+let valores = [5, 6, 7, 6, 9];
+
+let indice = 0;
+
+while(indice < valores.length){
+    console.log(valores[indice]);
+    indice++;
+};
+
+console.log("Terminado.");
+
+// Do While (Cuando el código debe ejecutarse al menos una vez)
+do {
+    console.log(valores[indice]);
+    indice++;
+
+} while(true)
