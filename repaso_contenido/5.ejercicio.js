c2919890da579d702f4a53b5c78067576fc3539e
Jorge <jltamayocabello@gmail.com>
2026-04-02 10:52:14 -0300

    Ejercicio 5

---
+++ repaso_contenido/5.ejercicio.js
@@ -0,0 +1,11 @@
+let total = 0;
+
+let valores = [5, 6, 7];
+
+for (let index = 0; index < valores.length; index++) {
+    const valor = valores[index];
+
+    total += valor;
+}
+
+console.log(total);
