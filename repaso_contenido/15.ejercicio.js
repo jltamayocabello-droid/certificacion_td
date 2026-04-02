90611e33e4ef2792dc532e0e081c8a237db704c1
Jorge <jltamayocabello@gmail.com>
2026-04-02 12:35:57 -0300

    Ejercicio 15

---
+++ repaso_contenido/15.ejercicio.js
@@ -0,0 +1,24 @@
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Incrementa el valor de la variable valorInicial
+ * 
+ * @return {undefined} No devuelve nada
+/*******  773cfedc-9799-4bb1-ba78-6571b02a4529  *******/
+function contador(){
+
+    let valorInicial = 0;
+    function aumentar(){
+        valorInicial++
+    }
+    function verContador(){
+        return valorInicial;
+    }
+    return{
+        aumentar,
+        verContador
+    }
+    
+};
+
+let contador1 = contador();
+console.log(contador.verContador());
