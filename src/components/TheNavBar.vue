2665b7b991f2d6d1d4fd00fe050beb13c7f5ea61
Jorge <jltamayocabello@gmail.com>
2026-04-04 19:01:48 -0300

    Se crea TheNavBar

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useUserStore } from '../stores/user';
+const userStore = useUserStore();
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="navbar__logo">TD-Certificación</div>
+    <div class="navbar__user">Hola, <strong>{{ userStore.name }}</strong></div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background: #2c3e50;
+  color: white;
+  margin-bottom: 2rem;
+}
+</style>
